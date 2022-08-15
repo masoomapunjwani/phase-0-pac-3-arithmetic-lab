14321954c79a78317fc6f483eb1d9cba73669729
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-08-15 19:55:37 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,54 @@
+function add(){
+    console.log(1+2);
+}
+
+add();
+
+function subtract(value1=2 , value2=3){
+    console.log(value1 + value2);
+}
+
+function multiply(number1 , number2){
+    return number1*number2;
+}
+
+
+function divide(){
+    console.log(3/7);
+}
+
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+
+function increment(n){
+    n++;
+    return (n);
+}
+
+function decrement(n){
+    n--;
+    return (n);
+}
+
+function makeInt(n){
+    return parseInt(n);
+}
+
+function makeInt(n){
+    return parseFloat(n);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+ 
